c3341deddb6d190f970d5050cdd54b29205ea2e6
unknown <you@example.com>
2022-05-16 20:47:58 +0700

    a

---
+++ ss7-bien-du-lieu-bien-toan-tu/thuc-hanh/a.js
@@ -0,0 +1,6 @@
+let inputWidth;
+let inputHeight;
+inputHeight = parseInt(prompt("Enter the width"));
+inputWidth = parseInt(prompt("Enter the width"));
+let F = inputWidth * inputHeight;
+document.write("F=" + F)
